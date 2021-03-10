107e7843db98483fc79b931a67483f05c6397d0b
jp-nullpod <jp.fortuno@nullpod.com>
2021-03-10 10:51:16 +0400

    [added] Environment Variables

--- nullweb/settings.py
+++ nullweb/settings.py
@@ -9,8 +9,12 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+env_path= Path('.') / '.env'
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'bmkmqcwy)5nr@l&mvwz42+o8hp&5*oui09p)$0w_@c0h0-=pib'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
