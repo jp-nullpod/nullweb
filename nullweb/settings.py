47e5bc86d8dae6af8e45a12b88f9b529a3e4d4ef
jp-nullpod <jp.fortuno@nullpod.com>
2021-03-16 20:27:25 +0400

    prod change

--- nullweb/settings.py
+++ nullweb/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-STATIC_URL = '/staticfiles/'
+STATIC_URL = 'staticfiles/'
 
 
 STATICFILES_FINDERS = [
